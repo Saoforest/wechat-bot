25a4be933a41bda3380fa363ebd92876c29d4dbb
XiaoLin <xiaolin@tiegangan.top>
2024-07-31 10:11:40 +0800

    feat: 添加自动刷新注解

--- wechat-bot-core/src/main/java/top/xiaolinz/wechat/bot/core/model/properties/BaseMessageListenerProperties.java
+++ wechat-bot-core/src/main/java/top/xiaolinz/wechat/bot/core/model/properties/BaseMessageListenerProperties.java
@@ -3,6 +3,7 @@ package top.xiaolinz.wechat.bot.core.model.properties;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * 基本插件属性
@@ -11,6 +12,7 @@ import lombok.Data;
  * @version 1.0.0
  * @date 2024/7/22
  */
+@RefreshScope
 @Data
 public abstract class BaseMessageListenerProperties {
     /**
